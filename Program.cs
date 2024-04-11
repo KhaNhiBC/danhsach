c94ee4ed481e548efdcba46de0b726f68d9156cd
KhaNhi <KhaNhi@users.noreply.github.com>
2024-04-11 20:19:24 +0700

    add

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<int> mylist = new List<int>(); // khoi tao list trong
+        for (int i = 0;i < 10;i++){
+            mylist.Add(i);
+        }
+        foreach (var item in mylist){
+            Console.Write(item+",");
+        }
+        mylist.RemoveAt(5);
+        Console.WriteLine();
+        foreach (var item in mylist){
+            Console.Write(item+",");
+        }
+    }
+    // count so phan tu bao gom trong list
+    // Add them phan tu vao list
+    // Removeat(index)
+    // clear xoa toan bo list
+}
